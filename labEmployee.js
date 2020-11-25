c589e69539aa8559c77e5601c051da150efefd24
asd908433289 <73798628+asd908433289@users.noreply.github.com>
2020-11-25 17:25:37 -0500

    Update labEmployee.js

--- labEmployee.js
+++ labEmployee.js
@@ -3,7 +3,7 @@ var con = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "FYPEX123456",
+    password: "aa41717,.",
     database: "virustest"
 });
 
@@ -12,6 +12,13 @@ const app = express();
 const url = require("url");
 const { Console } = require("console");
 
+
+app.get("/employeeLogin", (req, res) => {
+    writeLogin(req, res);
+});
+app.get("/employeeHome", (req, res) => {
+    writeHome(req, res);
+});
 app.get("/", (req, res) => { writeLabLogin(req, res) });
 
 app.get("/labHome", (req, res) => { writeLabHome(req, res); });
@@ -586,5 +593,155 @@ function writeWellTesting(req, res) {
 
 
 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function writeLogin(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    let query = url.parse(req.url, true).query;
+    query.email = "";
+    query.password = "";
+    let html = `<!DOCTYPE html>
+    
+    <html>
+    
+    <head>
+        <style type="text/css">
+            label {
+                display: inline-block;
+                width: 140px;
+                text-align: middle;
+            }
+    
+    
+        </style>
+    
+    
+    </head>
+    
+    <body>
+        <p>Employee Login Page for Results</p>
+        <form method="get"action="/employeeLogin">
+    
+            <label >Email:</label>
+            <input name="email" type="text"></input>
+            <br>
+            <label >Password:</label>
+            <input name="password" type="text"></input>
+            <br>
+         
+            
+    
+       
+       
+            <button type="submit" id="login" formaction="employeeHome">login</button>
+        </form>
+    
+    
+    </body>
+    
+    </html>`
+    res.write(html);
+    res.end();
+}
+function writeHome(req, res) {
+    let query = url.parse(req.url, true).query;
+    let Email = query.email
+    let sql = `SELECT * FROM employee
+                WHERE email= "`+ query.email + `"
+                AND password= "`+ query.password + `"`;
+
+    let html = ``
+
+
+
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+
+        if (result.length <= 0) {
+            html = `<!DOCTYPE html>
+            <html lang = "en">
+            <head>Error Page </head>
+            <br></br>
+            <body>
+                <p> The account: `+ query.email + ` or password: ` + query.password + ` is incorrect. </p>
+                
+                <br>
+                <form action="/employeeLogin" method="get"><button type="submit"> Try Again</button></form>
+            </body>
+            </html>`;
+            res.write(html);
+        res.end();
+
+        } else {
+            let sql2 = `select(SELECT T.collectionTime FROM employeetest T, employee E where E.EmployeeID=  (SELECT employeeid from employee where email="` + Email + `")and E.EmployeeID=T.employeeId)as collectionTime,(select wt.result from welltesting wt,poolmap pm where wt.poolbarcode=(select PM.poolbarcode from poolmap pm,employeetest t where pm.poolbarcode=(select t.testBarcode from employeetest T, EMPLOYEE E 
+                WHERE E.employeeID=(SELECT employeeid from employee where email="` + Email + `") and E.employeeID=t.employeeid) and pm.testbarcode=t.testbarcode) and wt.poolbarcode=pm.poolbarcode)as result  `;
+            
+            html = `<!DOCTYPE html>
+            <html>
+            
+            <head>
+                <p>Employee Home</p>
+                <style>
+                    table,th,td{
+                        border: 1px solid black;
+                    }
+                    table{
+                        border-collapse:collapse;
+                    }
+                
+                    </style>
+               
+            
+            </head>
+            
+            <body>
+                <table>
+                    <tr>
+                        <th >Collection Date</th>
+                        <th > Result</th>
+                        
+                    </tr>
+                    <tr>`;
+            con.query(sql2, function (err, result2) {
+                
+
+                for (let item of result2) {
+                    html += `<td>` + item.collectionTime + `</td>
+                    <td>`+item.result+`</td>
+                    </tr>`
+                }
+
+
+
+
+
+                html += `</table>
+                
+            
+                      </body>
+            
+            </html>`;
+                res.write(html);
+                res.end();
+
+            });
+
+            
+        }
+
+    });
 
 }
